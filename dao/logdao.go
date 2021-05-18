a87698ef15dcf0b6f2f870bbbfb3c49338288252
lcy <luchenyang2019@gmail.com>
2021-05-18 13:08:56 +0800

    bugFix

--- dao/logdao.go
+++ dao/logdao.go
@@ -20,8 +20,12 @@ func (dao LogDao) Write(logEntity *entity.LogEntity) {
 	table := logEntity.Level + time.Now().Format("200601")
 	dao.db.DB.Table(table).AutoMigrate(&entity.LogEntity{})
 	dao.db.DB.AutoMigrate(&entity.Item{})
-	dao.db.DB.Create(&entity.Item{
-		Name: table,
-	})
+	var count int64
+	dao.db.DB.Model(&entity.Item{}).Where("Name = ?", table).Count(&count)
+	if count == 0 {
+		dao.db.DB.Create(&entity.Item{
+			Name: table,
+		})
+	}
 	dao.db.DB.Table(table).Create(logEntity)
 }
